d32ff814c935020099cbd09e19f40c6166447dd3
DevAman-sudo <devaman.net@gmail.com>
2020-12-08 09:25:08 +0545

    First Commit , Basic Codes

---
+++ Js/main.js
@@ -0,0 +1,5 @@
+// JavaScript DOM Elements //
+const form = document.getElementById('form');
+const textArea = document.getElementById('text-area');
+const button = document.getElementById('button');
+const dataContainer = document.getElementById('data-container');
